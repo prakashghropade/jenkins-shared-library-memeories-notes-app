8c9744ce5ad10f5ce1501f20ab0348930d055dc7
prakashghropade <prakashghorpade2001@gmail.com>
2026-08-11 21:42:14 +0530

    added the notify build function

--- vars/dockerBuildPush.groovy
+++ vars/dockerBuildPush.groovy
@@ -5,13 +5,31 @@ def call(Map config = [:]) {
     def credentialsId = config.credentialsId
     def dockerfile = config.dockerfile ?: "Dockerfile"
     def context = config.context ?: "."
+    def apiUrl = config.apiUrl ?: ""
+
+     
 
     stage("Docker Build") {
+
+        if (apiUrl){
+
+            sh """
+                docker build \
+                    --build-arg REACT_APP_API_URL=${apiUrl} \
+                    -f ${dockerfile} \
+                    -t ${image}:${tag} \
+                    ${context}
+            
+            """
+        } else {
+
         sh """ 
             docker build \
-            -t ${dockerfile} ${image}:${tag} \
+            -f  ${dockerfile} \
+            -t ${image}:${tag} \
             ${context}
-        """ 
+           """ 
+        }
     }
 
     stage("Docker Push"){
