ec8675467c88278524d74f2828a6c5db39c2d045
prakashghropade <prakashghorpade2001@gmail.com>
2026-08-11 21:58:50 +0530

    updated the groovy syntax issue

--- vars/notifyBuild.groovy
+++ vars/notifyBuild.groovy
@@ -4,22 +4,19 @@ def call (Map config = [:]) {
     def status = config.status ?: currentBuild.currentResult
     def recipient = config.recipient
 
-    emailtext(
+    mail(
 
         to: recipient,
         subject: "${status}: ${env.JOB_NAME} #${env.BUILD_NUMBER}",
         body: """
-            <h2>Jenkins Build Notification</h2>
+                    CI Pipeline completed successfully.
 
-            <p><b>Job:</b> #${env.JOB_NAME}</p>
-            <p><b>Build:</b> #${env.BUILD_NUMBER}</p>
-            <p><b>Status:</b> ${status}</p>
-            <p><b>Branch:</b> ${env.BRANCH_NAME} ?: 'N/A'}</p>
-            <p><b>Build URL:</b> ${env.BUILD_URL}</p>
-        """,
-
-        mimeType: 'text/html'
-
+                    Job: ${env.JOB_NAME}    
+                    Build: ${env.BUILD_NUMBER}
+                    Status: SUCCESS
+                    Branch: ${env.BRANCH_NAME}
+                    Build URL: ${env.BUILD_URL}
+                    """,
     )
 
 }
